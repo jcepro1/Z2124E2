da3237bb2548f088863fd03344d092183c409879
jcepro1 <115606134+jcepro1@users.noreply.github.com>
2024-06-19 09:00:47 -0400

    RESPUESTA3.pas

--- RESPUESTA3.pas
+++ RESPUESTA3.pas
@@ -1,43 +1,29 @@
 //EJERCICIO 3
-
-function InvertirPalabra(palabra: string): string;
+function ejercicio3(cad: string): string;
 var
-  i: Integer;
-  invertida: string;
+  i: byte;
+  car, suma: string;
 begin
-  invertida := '';
-  for i := length(palabra) downto 1 do
-    invertida := invertida + palabra[i];
-  InvertirPalabra := invertida;
+  car := '';
+  suma := '';
+  cad := ' ' + cad;
+  for i := 1 to n - 1 do
+  begin
+    if cad[i] = ' ' then
+    begin
+      car := UpCase(cad[i + 1]);
+      suma := suma + car;
+    end
+    else
+    begin
+      suma := suma + cad[i + 1];
+    end;
+  end;
+
+  Result := suma;
 end;
 
-function InvertirUltimaPalabraEnCadena(cad: string): string;
-var
-  i, inicioUltimaPalabra: Integer;
-  ultimaPalabra, resultado: string;
-begin
-
-  i := length(cad);
-  while (i > 0) and (cad[i] = ' ') do
-    Dec(i);
-
-  inicioUltimaPalabra := i;
-  while (inicioUltimaPalabra > 0) and (cad[inicioUltimaPalabra] <> ' ') do
-    Dec(inicioUltimaPalabra);
-
-  if cad[inicioUltimaPalabra] = ' ' then
-    Inc(inicioUltimaPalabra);
-
-  ultimaPalabra := Copy(cad, inicioUltimaPalabra,
-    length(cad) - inicioUltimaPalabra + 1);
-
-  ultimaPalabra := InvertirPalabra(ultimaPalabra);
-
-  resultado := Copy(cad, 1, inicioUltimaPalabra - 1) + ultimaPalabra;
-
-  InvertirUltimaPalabraEnCadena := resultado;
-end;
 //LLAMADA
-Edit3.Text := InvertirUltimaPalabraEnCadena(cad);
+ Edit3.Text := ejercicio3(cad);
 
 
